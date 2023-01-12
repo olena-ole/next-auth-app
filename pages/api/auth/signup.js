5c3e64033045ef2bb3094e8e31076bb5f589eebc
lenna <pampam32121@gmail.com>
2023-01-12 17:47:05 +0200

    installed mongodb, bcryptjs and next-auth, added a user signup api route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,31 @@
+import { connectToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
+
+export default async function handler(req, res) {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+        !email ||
+        !email.includes('@') ||
+        !password ||
+        password.trim().length < 7
+    ) {
+        res.status(422).json({ message: 'Invalid input - password should also be at least 7 characters long.' })
+        return;
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+
+    const hashedPassword = hashPassword(password); // ??? await must be here ???
+
+    const result = await db.collection('users').insertOne({
+        email: email,
+        password: hashedPassword
+    });
+
+    res.status(201).json({ message: 'Created user!' });
+};
